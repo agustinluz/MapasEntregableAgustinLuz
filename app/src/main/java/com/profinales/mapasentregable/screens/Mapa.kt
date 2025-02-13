c3ff98d3ac7b79e33dbc59089c96b0b022cb44b0
agustinluz <agustinluzmartinll@gmail.com>
2025-02-13 20:27:06 +0100

    ESTO ES CHETO

--- app/src/main/java/com/profinales/mapasentregable/screens/Mapa.kt
+++ app/src/main/java/com/profinales/mapasentregable/screens/Mapa.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+//CHEETO
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
